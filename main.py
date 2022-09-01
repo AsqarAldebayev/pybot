64f64f91be3b26667c548e0e82f920a5181e499a
Asqar <49881259+AsqarAldebayev@users.noreply.github.com>
2022-09-01 19:49:59 +0500

    Bot boshlang'ich shakl

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-print("Hello, world")
+"""
+This is an echo bot.
+It echoes any incoming text messages.
+"""
+
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '5552257325:AAHVIuemiVUpOT7ry_UCWSledaz8ZeO05Ew'
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+class User():
+    def __init__(self):
+        self.name = None
+        self.userid = None
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    user = User()
+    user.name = message.chat.first_name
+    user.userid = message.chat.id
+    print(user.name, user.userid)
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
